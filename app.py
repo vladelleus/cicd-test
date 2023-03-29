e8144c6bdcb86141668f624927cc1fd07bb0e98d
Vlad Matsapura <vladelleus20001@gmail.com>
2023-03-29 20:41:53 +0300

    Adding requirements, fixing logic in app
    Adding tests

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import socket
+import subprocess
+import requests
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -7,7 +9,24 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b'Hello world from hostname: ' + socket.gethostname().encode())
 
-SERVER_PORT = 8000
-httpd = HTTPServer(('0.0.0.0', SERVER_PORT), SimpleHTTPRequestHandler)
-print('Listening on port %s ...' % SERVER_PORT)
-httpd.serve_forever()
+# Adding tests
+
+def test_server_response():
+    # Start the server in a separate process
+    server_process = subprocess.Popen(['python', 'app.py'])
+
+    # Make a request to the server
+    response = requests.get('http://127.0.0.1:8000')
+
+    # Stop the server process
+    server_process.terminate()
+
+    # Assert that the response is OK and contains the expected message
+    assert response.status_code == 200
+    assert 'Hello world' in response.text
+
+if __name__ == '__main__':
+    SERVER_PORT = 8000
+    httpd = HTTPServer(('0.0.0.0', SERVER_PORT), SimpleHTTPRequestHandler)
+    print('Listening on port %s ...' % SERVER_PORT)
+    httpd.serve_forever()
---
+++ test_app.py
@@ -0,0 +1,4 @@
+from app import test_server_response
+
+def test_my_app():
+    test_server_response()
