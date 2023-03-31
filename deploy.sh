542d2e56dbf1e7c8517d4eb17e3ef25b1b5257b8
Vlad Matsapura <vladelleus20001@gmail.com>
2023-03-31 12:52:22 +0300

    Fixing ci and deploy file

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,20 @@
 IMAGE_NAME=$1
 CONTAINER_NAME=myapp
 
+# Configuring AWS
+
+export AWS_SECRET_ACCESS_KEY=$2
+export AWS_ACCESS_KEY_ID=$3
+export AWS_DEFAULT_REGION=$4
+
+aws ecr get-login-password --region $4 | docker login --username AWS --password-stdin $5.dkr.ecr.$4.amazonaws.com
+
 # Pull the latest image from Docker Hub
 docker pull $IMAGE_NAME
 
 # Stop and remove the old container
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
+docker stop $(docker ps -lq)
+docker rm $(docker ps -lq)
 
 # Run the new container
 docker run -d --name $CONTAINER_NAME -p 8000:8000 $IMAGE_NAME
