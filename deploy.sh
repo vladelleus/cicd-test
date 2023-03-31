7036fb409b85e1385851a20394f0d58eba7b5a9a
Vlad Matsapura <vladelleus20001@gmail.com>
2023-03-31 13:03:12 +0300

    Fixing deploy file

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ export AWS_SECRET_ACCESS_KEY=$2
 export AWS_ACCESS_KEY_ID=$3
 export AWS_DEFAULT_REGION=$4
 
+# Configuring Docker to use ECR
+echo "The script was passed $# arguments"
 aws ecr get-login-password --region $4 | docker login --username AWS --password-stdin $5.dkr.ecr.$4.amazonaws.com
 
 # Pull the latest image from Docker Hub
